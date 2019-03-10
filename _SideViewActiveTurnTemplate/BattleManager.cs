d61fa2a73b7ea060c128a47578ca04d74dba4235
Dary1 <kunio.yufan@gmail.com>
2019-03-10 13:40:19 +0900

    Add files via upload

---
+++ _SideViewActiveTurnTemplate/BattleManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    public static BattleManager Instance;
+
+    public List<Unit> AllUnits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    public void PauseTime() {
+        AllUnits.ForEach(f => f.enabled = false);
+    }
+
+    public void ResumeTime() {
+        AllUnits.ForEach(f => f.enabled = true);
+    }
+}
---
+++ _SideViewActiveTurnTemplate/Unit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public int TurnWait = 300;
+    int TurnCount = 0;
+    public Animator Animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TurnCount++ == TurnWait) {
+            TurnCount = 0;
+            BattleManager.Instance.PauseTime();
+            this.Animator.Play("AttackEntry");
+            
+        }
+    }
+
+    public void ResumeTime() {
+        BattleManager.Instance.ResumeTime();
+    }
+}
